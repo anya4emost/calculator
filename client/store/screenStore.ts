dbc7eb8035bb66aec3d8c76ac6a12765c932c5c3
Анна Михайлова <annamihajlova@MacBook-Pro-Anna.local>
2017-08-25 16:24:03 +0300

    calculating interest

--- client/store/screenStore.ts
+++ client/store/screenStore.ts
@@ -8,11 +8,17 @@ const isCorrectKey = (key: any) => {
 
 export class Calculations {
     @observable screen: string = '';
+    @observable previous: string;
 
     @computed
     get displayedScreen() {
         return this.screen || '0';
     }
+
+    @computed
+    get previousCalculations() {
+        return this.previous
+    };
 
     hasDot = (screen: string) => {
         let a = screen.split(/[-+*/]/);
@@ -27,6 +33,9 @@ export class Calculations {
             this.screen = "0" + param;
             return
         }
+        if (this.screen == "0" && param == "0") {
+            return
+        }
         if (param == '.' && this.hasDot(this.screen)) {
             return
         }
@@ -35,35 +44,93 @@ export class Calculations {
 
     manualInput = (event: any) => {
 
-        if (!isCorrectKey(event.key)) {
-            return;
-        }
+        if (isCorrectKey(event.key)) {
+            if (event.key == '.' && this.hasDot(this.screen)) {
+                return
+            }
+            if (!this.screen && event.key == '.') {
+                this.screen = "0" + event.key;
+                return
+            }
 
-        if (event.key == '.' && this.hasDot(this.screen)) {
-            return
+            if (this.screen == "0" && event.key == "0") {
+                return
+            }
+            this.screen += event.key.toString();
         }
-        if (!this.screen && event.key == '.') {
-            this.screen = "0" + event.key;
-            return
-        }
-        this.screen += event.key.toString();
 
         if (event.key == "=" || event.keyCode == 13) {
             this.toCalculate();
         }
         if (event.keyCode == 8) {
             this.screen = this.screen.slice(0, -1);
-            if (this.screen.length == 0) {
-                this.screen = "0"
-            }
         }
     };
 
     reset = () => {
-        this.screen = "0";
+        if (this.screen) {
+            this.previous = this.screen;
+        }
+        this.screen = "";
+    };
+    calculatingInterest = () => {
+        if (this.screen.indexOf("%") == -1) return;
+        let a = this.screen.split(/[-+*/]/);
+        let p = -1;
+        let results: Array<number> = [];
+        let catPositions: Array<number> = [];
+        let lengthCatPositions: Array<number> = [];
+        a.forEach((item, i) => {
+                if (item.indexOf("%") !== -1) {
+                    p = this.screen.indexOf("%", p + 1);
+                    catPositions.push(p);
+                    let sign = this.screen.charAt(p - item.length);
+                    switch (sign) {
+                        case '+':
+                            let b = Number(a[i - 1]);
+                            let c = Number(a[i].slice(0, -1)) * b / 100;
+                            let d = b + c;
+                            console.log(d);
+                            results.push(d);
+                            lengthCatPositions.push(a[i].length + a[i - 1].length + 1);
+                            break;
+
+                        case '-':
+                            let e = Number(a[i - 1]);
+                            let f = Number(a[i].slice(0, -1)) * e / 100;
+                            let g = e - f;
+                            lengthCatPositions.push(a[i].length + a[i - 1].length + 1);
+                            results.push(g);
+                            break;
+
+                        case '*':
+                            let h = Number(a[i].slice(0, -1)) / 100;
+                            lengthCatPositions.push(a[i].length);
+                            results.push(h);
+                            break;
+
+                        default:
+                            let j = Number(a[i].slice(0, -1)) * 100;
+                            lengthCatPositions.push(a[i].length);
+                            results.push(j);
+                            break
+                    }
+                }
+            }
+        );
+        for (let i = results.length - 1; i > -1; i--) {
+            let begin = this.screen.substr(0, catPositions[i] - lengthCatPositions[i] + 1);
+            let end = this.screen.substr(catPositions[i] + 1);
+            this.screen = begin + results[i].toString() + end;
+        }
+        return this.screen;
     };
     toCalculate = () => {
-        this.screen = eval(this.screen);
+        if (this.screen) {
+            this.previous = this.screen + "=";
+        }
+        this.calculatingInterest();
+        this.screen = eval(this.screen).toString();
     }
 }
 
--- client/views/ScreenComponent.tsx
+++ client/views/ScreenComponent.tsx
@@ -7,8 +7,12 @@ import {Calculations} from "../store/screenStore";
 export class Screen extends React.Component<Props, {}> {
     render() {
         return (
-            <input id="screen" className="screen" value={this.props.calculations.displayedScreen} onKeyUp={this.props.calculations.manualInput}>
-            </input>
+            <div className="display">
+                <span className="previous_calculations">{this.props.calculations.previousCalculations}</span>
+                <input id="screen" className="screen" value={this.props.calculations.displayedScreen}
+                       onKeyUp={this.props.calculations.manualInput}>
+                </input>
+            </div>
         );
     }
 }
